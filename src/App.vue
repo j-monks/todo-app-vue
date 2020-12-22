5c1db7f1a0b266ba2baf4b6a1beced6e5690556b
James Monks <J.monks23@gmail.com>
2020-12-22 20:43:27 +0000

    added newToDo property and binded it to the input which is stored as a ref

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,29 @@
   <h1>Vue 3 Todo App</h1>
   <form @submit.prevent="addNewTodo">
     <label>New Todo</label>
-    <input name="newTodo" />
+    <input v-model="newTodo" name="newTodo" />
     <button>Add New Todo</button>
   </form>
+  <h2>{{ newTodo }}</h2>
 </div>
 </template>
 
 <script>
+// creates properties/variables/objects that respond to change and you can know when those things are changed
+import { ref } from 'vue';
+
 export default {
   setup() {
+    // newTodo is similar to having a property in data that is initialized to an empty string 
+    const newTodo = ref("");
+
     function addNewTodo() {
       console.log("Form was submitted");
     }
 
     return {
+      // exposing variables to the template
+      newTodo,
       addNewTodo,
     };
   }
